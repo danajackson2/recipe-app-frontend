ef60d4d789a254c5499559a3f14f3af50c935c90
bswan0002 <bswan0002@gmail.com>
2021-04-30 10:37:21 -0700

    nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+// Libraries
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 // Styles
 import "./App.css";
 import "./custom.scss";
 // Components
+import TopNav from "./components/TopNav";
 import Footer from "./components/Footer.js";
 
 function App() {
+  const [user, setUser] = useState({});
   return (
     <>
-      <div className="body">body</div>
+      <div className="body">
+        <Router>
+          <TopNav user={user} />
+        </Router>
+      </div>
       <Footer />
     </>
   );
