a1608a5399f852cc7a16203394dea38c8fcd156e
danajackson2 <danajackson2@gmail.com>
2021-04-30 12:40:53 -0700

    login signup

--- src/App.js
+++ src/App.js
@@ -12,15 +12,38 @@ import "./custom.scss";
 // Components
 import TopNav from "./components/TopNav";
 import Footer from "./components/Footer.js";
-//dana git push test
 
 function App() {
   const [user, setUser] = useState({});
+  
+  function signUp(username, password){
+    fetch(`${process.env.REACT_APP_BASE_URL}/users`,{
+      method: 'POST',
+      headers: {'content-type':'application/json'},
+      body: JSON.stringify({new_user: {username, password}})
+  })
+  .then(res => res.json())
+  .then(console.log)
+  // .then(data => handleAuthResponse(data))
+  }
+
+  function signIn(username, password){
+    fetch(`${process.env.REACT_APP_BASE_URL}/signin`,{
+      method: 'POST',
+      headers: {'content-type':'application/json'},
+      body: JSON.stringify({username, password})
+  })
+  .then(res => res.json())
+  .then(console.log)
+  // .then(data => handleAuthResponse(data))
+  }
+
+
   return (
     <>
       <div className="body">
         <Router>
-          <TopNav user={user} />
+          <TopNav user={user} signIn={signIn} signUp={signUp}/>
         </Router>
       </div>
       <Footer />
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,27 +1,58 @@
-import { Navbar, Nav } from "react-bootstrap";
+import React, { useState } from 'react'
+import { Navbar, Nav, Dropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
-const TopNav = ({ user }) => {
+const TopNav = ({ user, signIn, signUp }) => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  
+  function emptyInput(e, type){
+    if(!e){
+      document.querySelectorAll(`.input-item.${type}`).forEach(i => i.value = '')
+      setUsername('')
+      setPassword('')
+    }
+  }
+
   return (
     <>
-      <Navbar bg="primary">
+      <Navbar bg="primary" className='justify-content-between'>
         <LinkContainer exact to="/">
           <Navbar.Brand>RecipeApp</Navbar.Brand>
         </LinkContainer>
         {"username" in user ? (
           <Nav>
             <LinkContainer to="sign-up">
-              <Nav.Link>Sign Up</Nav.Link>
+              <Nav.Link>Sign Out</Nav.Link>
             </LinkContainer>
           </Nav>
         ) : (
           <Nav>
-            <LinkContainer to="sign-in">
-              <Nav.Link>Sign In</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="sign-up">
-              <Nav.Link>Sign Up</Nav.Link>
-            </LinkContainer>
+            <Dropdown onToggle={e => emptyInput(e, 'sign-in')}>
+              <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                Sign In
+              </Dropdown.Toggle>
+              <Dropdown.Menu align="right">
+                <div className='login-div'>
+                  <input onChange={e => setUsername(e.target.value)} className='input-item sign-in' placeholder='username'/>
+                  <input onChange={e => setPassword(e.target.value)} className='input-item sign-in' placeholder='password' type='password'/>
+                  <button onClick={() => signIn(username, password)} className='btn'>Submit</button>
+                </div>
+              </Dropdown.Menu>
+            </Dropdown>
+            <Dropdown onToggle={e => emptyInput(e, 'sign-up')}>
+              <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                Sign Up
+              </Dropdown.Toggle>
+              <Dropdown.Menu align="right">
+                <div className='login-div'>
+                  <input onChange={e => setUsername(e.target.value)} className='input-item sign-up' placeholder='username'/>
+                  <input onChange={e => setPassword(e.target.value)} className='input-item sign-up' placeholder='password' type='password'/>
+                  <button onClick={() => signUp(username, password)} className='btn'>Submit</button>
+                </div>
+              </Dropdown.Menu>
+            </Dropdown>
           </Nav>
         )}
       </Navbar>
