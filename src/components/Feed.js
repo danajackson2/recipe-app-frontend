9b86f53c9ed9fe00fd7f3e1d88ca6ff49a0cfe3c
bswan0002 <bswan0002@gmail.com>
2021-05-01 10:21:11 -0700

    stuff

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -8,14 +8,23 @@ import FeedCard from "./feed_components/FeedCard";
 
 const Feed = ({ recipes, user_id, fetchRecipes }) => {
   const [formOpen, setFormOpen] = useState(false);
+  const [filter, setFilter] = useState("");
+  const [sort, setSort] = useState("likeDesc");
 
   const renderFeedCards = () => {
-    return recipes.map((r) => <FeedCard recipe={r} />);
+    let sorted;
+    if (sort === "likeDesc") {
+      sorted = recipes.sort((a, b) => b.likes - a.likes);
+    }
+    if (sort === "likeAsc") {
+      sorted = recipes.sort((a, b) => a.likes - b.likes);
+    }
+    return sorted.map((r) => <FeedCard key={r.id} recipe={r} />);
   };
 
   return (
     <>
-      <FeedBar formOpen={formOpen} setFormOpen={setFormOpen} />
+      {user_id && <FeedBar formOpen={formOpen} setFormOpen={setFormOpen} />}
       {formOpen && <RecipeForm user_id={user_id} fetchRecipes={fetchRecipes} />}
       <Container className="mt-4">
         <h1>Latest Recipes</h1>
