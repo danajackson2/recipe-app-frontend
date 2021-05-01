00278214f72ee2594cd9375bbde55c9da0750abb
bswan0002 <bswan0002@gmail.com>
2021-04-30 18:00:27 -0700

    form done

--- src/components/feed_components/RecipeForm.js
+++ src/components/feed_components/RecipeForm.js
@@ -1,14 +1,53 @@
+// Libraries
 import { Container, Form, Col, Button, Card } from "react-bootstrap";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinusSquare } from "@fortawesome/free-regular-svg-icons";
 
-const RecipeForm = () => {
+const RecipeForm = (user = { id: 1 }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
   const [ingredients, setIngredients] = useState({
     0: { name: "", unit_of_measure: "", quantity: 0 },
   });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`${process.env.REACT_APP_BASE_URL}/recipes`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: user.id,
+        name: title,
+        description: description,
+        recipe_items: ingredients,
+        instructions: instructions,
+        image_url: imageUrl,
+      }),
+    });
+  };
+
+  const unitsOfMeasurement = [
+    "N/A",
+    "Tbsp",
+    "Tsp",
+    "Cups",
+    "Pints",
+    "Quarts",
+    "Gallons",
+    "Ounces",
+    "Pounds",
+  ];
+
+  const renderOptions = () => {
+    return unitsOfMeasurement.map((unit) => {
+      return <option key={`${unit}-${Math.random()}`}>{unit}</option>;
+    });
+  };
 
   const handleIngredientChange = (e) => {
     setIngredients({
@@ -37,12 +76,13 @@ const RecipeForm = () => {
     });
   };
 
-  const removeIngredient = (e) => {
+  const removeIngredient = (rowKey) => {
+    console.log(rowKey);
     let newIngredients = {};
     for (let index = 0; index < Object.keys(ingredients).length; index++) {
-      if (index < e.target.id) {
+      if (index < parseInt(rowKey)) {
         newIngredients[index] = ingredients[index];
-      } else if (index > e.target.id) {
+      } else if (index > parseInt(rowKey)) {
         newIngredients[index - 1] = ingredients[index];
       }
     }
@@ -63,11 +103,15 @@ const RecipeForm = () => {
           </Col>
           <Col xs={4}>
             <Form.Control
+              as="select"
               id={key}
-              name="quantity"
+              name="unit_of_measure"
+              options={unitsOfMeasurement}
               value={ingredients[key].unit_of_measure}
               onChange={handleIngredientChange}
-            ></Form.Control>
+            >
+              {renderOptions()}
+            </Form.Control>
           </Col>
           <Col xs={3}>
             <Form.Control
@@ -80,7 +124,7 @@ const RecipeForm = () => {
           <Col xs={1} className="icon-col">
             <FontAwesomeIcon
               id={key}
-              onClick={removeIngredient}
+              onClick={() => removeIngredient(key)}
               icon={faMinusSquare}
               size="lg"
               className="recipeform-minus mt-2"
@@ -94,7 +138,7 @@ const RecipeForm = () => {
   return (
     <Container>
       <Card className="px-4 pb-4 mt-4">
-        <Form className="mt-4">
+        <Form className="mt-4" onSubmit={handleSubmit}>
           <Form.Row>
             <h2 className="orange">New Recipe</h2>
           </Form.Row>
@@ -118,7 +162,7 @@ const RecipeForm = () => {
               <h5>Description:</h5>
               <p className="recipeform-remainingchars">
                 {140 - description.length}{" "}
-                {140 - description.length == 1 ? "char" : "chars"} remaining
+                {140 - description.length === 1 ? "char" : "chars"} remaining
               </p>
             </Form.Label>
             <Col xs={9}>
@@ -157,11 +201,11 @@ const RecipeForm = () => {
             </Col>
           </Form.Row>
           {renderIngredientFields()}
-          <Form.Row className="mt-3 d-flex justify-content-end">
+          <div className="mt-3 d-flex justify-content-end">
             <Button variant="secondary" onClick={addIngredient}>
               Add Ingredient
             </Button>
-          </Form.Row>
+          </div>
           <hr />
           <Form.Row>
             <Col>
@@ -171,18 +215,33 @@ const RecipeForm = () => {
           <Form.Row className="mb-3">
             <Col>
               <Form.Control
-                size="lg"
                 as="textarea"
                 rows={4}
                 placeholder="Teach your friends how to make this at home"
-                value={description}
-                onChange={handleDescriptionChange}
+                value={instructions}
+                onChange={(e) => setInstructions(e.target.value)}
               ></Form.Control>
             </Col>
           </Form.Row>
-          <Form.Row className="d-flex justify-content-end">
-            <Button type="submit">Submit</Button>
+          <hr />
+          <Form.Row>
+            <Col xs={4} md={3} lg={2}>
+              <Form.Label>
+                <h5>Image URL:</h5>
+              </Form.Label>
+            </Col>
+            <Col xs={8} md={9} lg={10}>
+              <Form.Control
+                type="text"
+                placeholder="Pics or it didn't happen"
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+              ></Form.Control>
+            </Col>
           </Form.Row>
+          <div className="d-flex justify-content-end mt-2">
+            <Button type="submit">Submit</Button>
+          </div>
         </Form>
       </Card>
     </Container>
